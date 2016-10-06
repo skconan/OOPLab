9f6bf4ab690e886747d5ef95f3e33519bd9f1b61
skconan <tulconan@gmail.com>
2016-10-06 22:20:34 +0700

    rm folder

--- core/src/com/mygdx/game/PacmanGame.java
+++ core/src/com/mygdx/game/PacmanGame.java
@@ -1,20 +1,15 @@
 package com.mygdx.game;
-
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.Game;
 
 public class PacmanGame extends Game {
-    public SpriteBatch batch;
+    public static SpriteBatch batch;
  
     public void create () {
         batch = new SpriteBatch();
         setScreen(new GameScreen(this));
     }
- 
+    
    
  
     @Override
