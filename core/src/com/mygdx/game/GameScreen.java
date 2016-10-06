fc923a9cd87c2c093bdc48783990c6b70e3dc0e2
skconan <tulconan@gmail.com>
2016-10-06 21:56:43 +0700

    Divide Class GameSrceen

---
+++ core/src/com/mygdx/game/GameScreen.java
@@ -0,0 +1,16 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class GameScreen extends ScreenAdapter {
+	 
+    private PacmanGame pacmanGame;
+ 
+    public GameScreen(PacmanGame pacmanGame) {
+        this.pacmanGame = pacmanGame;
+    }
+    @Override
+    public void render(float delta) {
+        System.out.println("Hello " + delta);
+    }
+}
