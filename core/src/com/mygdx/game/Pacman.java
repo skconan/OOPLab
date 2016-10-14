2030c9a47785ca87cca832e4e99031d6c4b52f82
skconan <supakit.kr@gmail.com>
2016-10-14 10:13:21 +0700

    edit camMoveInDirection pacman

--- core/src/com/mygdx/game/Pacman.java
+++ core/src/com/mygdx/game/Pacman.java
@@ -7,7 +7,6 @@ public class Pacman {
     private int currentDirection;
     private int nextDirection;
     private Maze maze;
-    private int currentRow, currentCol;
     public static final int DIRECTION_STILL = 0;
     public static final int DIRECTION_UP = 1;
     public static final int DIRECTION_RIGHT = 2;
@@ -39,12 +38,16 @@ public class Pacman {
     public void setNextDirection(int dir) {
         nextDirection = dir;
     }
+    private int getRow() {
+        return ((int)position.y) / WorldRenderer.BLOCK_SIZE; 
+    }
+ 
+    private int getColumn() {
+        return ((int)position.x) / WorldRenderer.BLOCK_SIZE; 
+    }
     private boolean canMoveInDirection(int dir) {
-    	int blockSize = WorldRenderer.BLOCK_SIZE;
-    	currentRow = (int) (position.y - blockSize/2)/blockSize;
-    	currentCol = (int) (position.x - blockSize/2)/blockSize;
-        int newRow = currentRow + DIR_DIFF[dir][1];
-        int newCol = currentCol + DIR_DIFF[dir][0];
+    	int newRow = getRow() + DIR_DIFF[dir][1];
+        int newCol = getColumn() + DIR_DIFF[dir][0];
         return !maze.hasWallAt(newRow, newCol);   
     }
     public void update() {
@@ -55,8 +58,8 @@ public class Pacman {
     		else {
                 currentDirection = DIRECTION_STILL;    
             }
-    		if(maze.hasDotAt(currentRow, currentCol)){
-    			maze.removeDotAt(currentRow, currentCol);
+    		if(maze.hasDotAt(getRow(), getColumn())){
+    			maze.removeDotAt(getRow(), getColumn());
     		}
     	}
     	position.x += SPEED * DIR_DIFF[currentDirection][0];
